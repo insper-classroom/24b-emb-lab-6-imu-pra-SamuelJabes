86ffef483baa1dcd7a0554f7d80a50d70e5fc6a0
samueljcc <samueljcc@al.insper.edu.br>
2024-10-10 13:22:52 +0000

    add lab6

--- main/main.c
+++ main/main.c
@@ -12,9 +12,20 @@
 
 #include <Fusion.h>
 
+#define SAMPLE_PERIOD (0.01f)
+
+#define UART_ID uart0
+
 const int MPU_ADDRESS = 0x68;
 const int I2C_SDA_GPIO = 4;
 const int I2C_SCL_GPIO = 5;
+
+QueueHandle_t xQueueMouse;
+
+typedef struct {
+    int8_t x;
+    int8_t y;
+} mouse_data_t;
 
 static void mpu6050_reset() {
     // Two byte reset. First byte register, second byte data
@@ -68,25 +79,81 @@ void mpu6050_task(void *p) {
     mpu6050_reset();
     int16_t acceleration[3], gyro[3], temp;
 
+    // Inicializar o Fusion
+    FusionAhrs ahrs;
+    FusionAhrsInitialise(&ahrs);
+
     while(1) {
         mpu6050_read_raw(acceleration, gyro, &temp);
-        printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
-        printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
-        printf("Temp. = %f\n", (temp / 340.0) + 36.53);
 
-        vTaskDelay(pdMS_TO_TICKS(10));
+        // Converter os dados para as unidades necessárias
+        FusionVector gyroscope = {
+            .axis.x = gyro[0] / 131.0f,  // Conversão para graus/s
+            .axis.y = gyro[1] / 131.0f,
+            .axis.z = gyro[2] / 131.0f,
+        };
+
+        FusionVector accelerometer = {
+            .axis.x = acceleration[0] / 16384.0f,  // Conversão para g
+            .axis.y = acceleration[1] / 16384.0f,
+            .axis.z = acceleration[2] / 16384.0f,
+        };
+
+        // Atualizar os dados de fusão de sensores (sem magnetômetro)
+        FusionAhrsUpdateNoMagnetometer(&ahrs, gyroscope, accelerometer, SAMPLE_PERIOD);
+
+        // Obter a orientação (roll, pitch e yaw)
+        const FusionEuler euler = FusionQuaternionToEuler(FusionAhrsGetQuaternion(&ahrs));
+
+        // Eixo X
+        int x = (int) (euler.angle.pitch) * (-1);
+        uart_putc_raw(UART_ID, 0);
+        uart_putc_raw(UART_ID, (x >> 8) & 0xFF);
+        uart_putc_raw(UART_ID, x & 0xFF);
+        uart_putc_raw(UART_ID, -1);
+
+        // Eixo Y
+
+        int y = (int) (euler.angle.roll) * (-1);
+        uart_putc_raw(UART_ID, 1);
+        uart_putc_raw(UART_ID, (y >> 8) & 0xFF);
+        uart_putc_raw(UART_ID, y & 0xFF);
+        uart_putc_raw(UART_ID, -1);
+
+        // printf("Acc. X = %d, Y = %d, Z = %d - ", acceleration[0], acceleration[1], acceleration[2]);
+        // printf("Gyro. X = %d, Y = %d, Z = %d - ", gyro[0], gyro[1], gyro[2]);
+        // printf("Temp. = %f\n\n\n", (temp / 340.0) + 36.53);
+
+        int acc;
+
+        if (acceleration[1] < 0) {
+            acc = -1 * acceleration[1];
+        } else {
+            acc = acceleration[1];
+        }
+
+        
+        if (acc > 17000){
+            // printf("Mouse click detectado!\n");
+            uart_putc_raw(UART_ID, 2);
+            uart_putc_raw(UART_ID, 0);
+            uart_putc_raw(UART_ID, ((acc >> 8) & 0xFF));
+            uart_putc_raw(UART_ID, -1);
+        }
+
+    
+        vTaskDelay(pdMS_TO_TICKS(10)); 
     }
-
-
 }
 
-int main() {
+void main() {
     stdio_init_all();
 
-    xTaskCreate(mpu6050_task, "mpu6050_Task 1", 8192, NULL, 1, NULL);
+    xQueueMouse = xQueueCreate(10, sizeof(mouse_data_t));
+
+    xTaskCreate(mpu6050_task, "mpu6050_task", 4096, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
-    while (true)
-        ;
+    while (true);
 }
